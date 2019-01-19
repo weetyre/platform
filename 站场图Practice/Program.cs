5c9547d64969523efe81c88d5103d0fc46b72ba6
cw <296083364@qq.com>
2019-01-19 11:51:26 +0800

    加了学号

--- 站场图Practice/Program.cs
+++ 站场图Practice/Program.cs
@@ -12,7 +12,7 @@ namespace 站场图Practice
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main()//16301032
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
